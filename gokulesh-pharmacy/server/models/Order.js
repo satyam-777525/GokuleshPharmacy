333fd3b364b23d3e07b84b676c10c8510ac94bb5
satyam-777525 <sv1061211@gmail.com>
2026-03-10 22:25:02 +0530

    change in order status

--- gokulesh-pharmacy/server/models/Order.js
+++ gokulesh-pharmacy/server/models/Order.js
@@ -28,7 +28,7 @@ const orderSchema = new mongoose.Schema({
   status: {
     type: String,
     enum: ['pending', 'confirmed', 'processing', 'shipped', 'delivered', 'cancelled'],
-    default: 'pending'
+    default: 'confirmed'
   },
   paymentMethod: { type: String, default: 'COD' },
   paymentDetails: {
